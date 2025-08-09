2cf231895e9a8de5f916868a5feddd269a9ff00b
ch4njun <38906956+slolee@users.noreply.github.com>
2025-08-09 14:50:01 +0900

    Add response field for search ingredients api (#27)

--- src/main/java/dayum/dayumserver/application/ingredient/dto/IngredientResponse.java
+++ src/main/java/dayum/dayumserver/application/ingredient/dto/IngredientResponse.java
@@ -2,9 +2,27 @@ package dayum.dayumserver.application.ingredient.dto;
 
 import dayum.dayumserver.domain.ingredient.Ingredient;
 
-public record IngredientResponse(Long id, String name) {
+public record IngredientResponse(
+    Long id,
+    String name,
+    String standardQuantity,
+    Double calories,
+    Double carbohydrates,
+    Double proteins,
+    Double fats,
+    Double sugars,
+    Double sodium) {
 
   public static IngredientResponse from(Ingredient ingredient) {
-    return new IngredientResponse(ingredient.id(), ingredient.name());
+    return new IngredientResponse(
+        ingredient.id(),
+        ingredient.name(),
+        ingredient.standardQuantity(),
+        ingredient.calories(),
+        ingredient.carbohydrates(),
+        ingredient.proteins(),
+        ingredient.fats(),
+        ingredient.sugars(),
+        ingredient.sodium());
   }
 }
