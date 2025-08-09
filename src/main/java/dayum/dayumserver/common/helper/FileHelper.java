d671530297074298b36afe98f08b5cdc7d3f103d
ch4njun <38906956+slolee@users.noreply.github.com>
2025-08-10 04:25:03 +0900

    Improve contents anaylze performence (#34)

--- src/main/java/dayum/dayumserver/common/helper/FileHelper.java
+++ src/main/java/dayum/dayumserver/common/helper/FileHelper.java
@@ -2,6 +2,8 @@ package dayum.dayumserver.common.helper;
 
 import dayum.dayumserver.application.common.exception.AppException;
 import dayum.dayumserver.application.common.exception.CommonExceptionCode;
+
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,6 +11,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.UUID;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.FileImageOutputStream;
 
 public class FileHelper {
 
@@ -28,6 +36,19 @@ public class FileHelper {
       Files.walk(path).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
     } catch (IOException e) {
       throw new AppException(CommonExceptionCode.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  public static void writeJpeg(BufferedImage image, File out, float q) throws IOException {
+    ImageWriter w = ImageIO.getImageWritersByFormatName("jpg").next();
+    ImageWriteParam p = w.getDefaultWriteParam();
+    p.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+    p.setCompressionQuality(q);
+    try (FileImageOutputStream fos = new FileImageOutputStream(out)) {
+      w.setOutput(fos);
+      w.write(null, new IIOImage(image, null, null), p);
+    } finally {
+      w.dispose();
     }
   }
 }
