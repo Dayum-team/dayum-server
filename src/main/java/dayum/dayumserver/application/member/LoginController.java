472468be7627f3c5c94b8166b16c42e5884362c4
sgus0608 <97722814+sgus0608@users.noreply.github.com>
2025-08-10 01:57:55 +0900

    Refactor/login api (#29)

--- src/main/java/dayum/dayumserver/application/member/LoginController.java
+++ src/main/java/dayum/dayumserver/application/member/LoginController.java
@@ -2,6 +2,7 @@ package dayum.dayumserver.application.member;
 
 import dayum.dayumserver.application.member.dto.LoginRequest;
 import dayum.dayumserver.application.member.dto.LoginResponse;
+import dayum.dayumserver.application.member.dto.RegisterRequest;
 import dayum.dayumserver.client.s3.oauth2.naver.NaverOAuthClient;
 import dayum.dayumserver.domain.member.Oauth2Provider;
 import lombok.RequiredArgsConstructor;
@@ -11,17 +12,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/auth")
 @RequiredArgsConstructor
-public class NaverLoginController {
+public class LoginController {
 
   private final MemberService memberService;
   private final NaverOAuthClient naverOAuthClient;
   private final JwtProvider jwtProvider;
 
+  @PostMapping("/signup/{provider}")
+  public ResponseEntity<LoginResponse> login(
+      @PathVariable String provider, @RequestBody RegisterRequest request) {
+    Oauth2Provider oauth2Provider = Oauth2Provider.from(provider.toUpperCase());
+    LoginResponse response = memberService.signup(request, oauth2Provider);
+    return ResponseEntity.ok(response);
+  }
+
   @PostMapping("/login/{provider}")
   public ResponseEntity<LoginResponse> login(
       @PathVariable String provider, @RequestBody LoginRequest request) {
     Oauth2Provider oauth2Provider = Oauth2Provider.from(provider.toUpperCase());
-    LoginResponse response = memberService.login(request, oauth2Provider);
-    return ResponseEntity.ok(response);
+
+    return memberService
+        .login(request.accessToken(), oauth2Provider)
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.status(401).build()); // 미가입 or 탈퇴 -> 401
   }
 }
