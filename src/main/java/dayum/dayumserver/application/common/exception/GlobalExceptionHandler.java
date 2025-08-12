af51b4640ac65e2291eec9098bcaa7f2c4a84591
ch4njun <38906956+slolee@users.noreply.github.com>
2025-08-12 22:57:27 +0900

    Integrate sentry for error monitoring (#38)

--- src/main/java/dayum/dayumserver/application/common/exception/GlobalExceptionHandler.java
+++ src/main/java/dayum/dayumserver/application/common/exception/GlobalExceptionHandler.java
@@ -1,17 +1,21 @@
 package dayum.dayumserver.application.common.exception;
 
+import dayum.dayumserver.application.common.response.ErrorResponse;
+import io.sentry.Sentry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import dayum.dayumserver.application.common.response.ErrorResponse;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(AppException.class)
   public ResponseEntity<ErrorResponse> handleAppException(AppException exception) {
+    log.error(exception.getMessage(), exception);
     return ResponseEntity.status(exception.getExceptionCode().getStatus())
         .body(ErrorResponse.of(exception));
   }
@@ -19,8 +23,14 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(HttpMessageNotReadableException.class)
   public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(
       HttpMessageNotReadableException e) {
-
     AppExceptionCode code = CommonExceptionCode.REQUEST_BODY_MISSING;
     return ResponseEntity.status(code.getStatus()).body(ErrorResponse.of(code));
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+    log.error("Internal server error!", e);
+    Sentry.captureException(e);
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
   }
 }
